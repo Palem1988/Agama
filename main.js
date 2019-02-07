09e1ba105aae706dea1e9aac02265279330f12c9
Asher Dawes <asher.dawes@gmail.com>
2019-02-06 19:19:33 -0800

    fixing icon

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ module.exports = guiapp;
 let agamaIcon;
 
 if (os.platform() === 'linux') {
-	agamaIcon = path.join(__dirname, 'assets/icons/agama_app_icon.icns');
+	agamaIcon = path.join(__dirname, 'assets/icons/agama_icons/256x256.png');
 }
 if (os.platform() === 'win32') {
 	agamaIcon = path.join(__dirname, '/assets/icons/agama_app_icon.ico');
